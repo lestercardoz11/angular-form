8df5728d2df8fbe90ccb9a13239d09dcb046f20c
Lester Cardoz <lestercardoz11@gmail.com>
2021-06-03 23:46:48 +0530

    Fixed email validation

--- src/app/entry/entry-control.service.ts
+++ src/app/entry/entry-control.service.ts
@@ -8,13 +8,18 @@ export class EntryControlService {
 
   toFormGroup(entries: EntryBase<string>[]) {
     const group: any = {};
+    const emailRegexExp =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     entries.forEach((entry) => {
       group[entry.key] = entry.required
         ? entry.key === 'email'
           ? new FormControl(
               entry.value || '',
-              Validators.compose([Validators.required, Validators.email])
+              Validators.compose([
+                Validators.required,
+                Validators.pattern(emailRegexExp),
+              ])
             )
           : new FormControl(entry.value || '', Validators.required)
         : new FormControl(entry.value || '');
